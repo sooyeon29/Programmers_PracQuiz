a011ac09943d1fdb9e770b677d8e296890cd8318
sooyun <so__oyeonii@naver.com>
2022-09-25 01:03:42 +0900

    [9/24] 프로그래머스문제풀기

--- 03_가운데글자가져오기.js
+++ 03_가운데글자가져오기.js
@@ -2,10 +2,10 @@
 
 function solution(s) {
     var answer = '';
-    if (s.length%2 == 0) {
+    if (s.length%2 === 0) {
         answer = s.substr(s.length/2 -1,2)
     } else {
-        return s.substr(s.length/2,1)
+        return s.substr(Math.floor(s.length/2),1)
     }
     return answer;
 }
